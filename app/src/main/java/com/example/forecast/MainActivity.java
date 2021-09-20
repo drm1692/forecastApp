4d34c36a6b034c05acbd19e117bcb33b32c494b5
Divy <majithiyadivy>
2021-09-20 18:01:54 +0530

    forecast

--- app/src/main/java/com/example/forecast/MainActivity.java
+++ app/src/main/java/com/example/forecast/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.forecast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Criteria;
@@ -15,6 +18,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -31,6 +36,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.squareup.picasso.provider.PicassoProvider;
 
@@ -47,24 +59,27 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout homeRl;
     private ProgressBar loadingPB;
-    private TextView cityTV,temperatureTv,conditionTV;
+    private TextView cityTV, temperatureTv, conditionTV;
     private TextInputEditText cityEdt;
-    private ImageView backIv,iconIV,searchIV;
+    private ImageView backIv, iconIV, searchIV;
     private RecyclerView weatherRv;
     private ArrayList<weatherRVModal> weatherRVModalArrayList;
     private weatherRVAdapter weatherRVAdapter;
 
-    private LocationManager locationManager;
-    int PERMISSION_CODE = 1;
+    FusedLocationProviderClient mfusedLocationClient;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    int PERMISSION_CODE = 44;
     private String cityName;
-    public Criteria criteria;
-    public String bestProvider;
 
+    final long MIN_TIME = 5000;
+    final float MIN_DISTANCE = 1000;
+    String Location_provider = LocationManager.GPS_PROVIDER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
 
         homeRl = findViewById(R.id.RLHome);
@@ -79,39 +94,22 @@ public class MainActivity extends AppCompatActivity {
         weatherRv = findViewById(R.id.RVWeather);
 
         weatherRVModalArrayList = new ArrayList<>();
-        weatherRVAdapter = new weatherRVAdapter(this,weatherRVModalArrayList);
+        weatherRVAdapter = new weatherRVAdapter(this, weatherRVModalArrayList);
         weatherRv.setAdapter(weatherRVAdapter);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-         ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        mfusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
 
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE);
-
-        }
-//        criteria = new Criteria();
-//        bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-       // if(location != null) {
-            cityName = getCityName(location.getLongitude(), location.getLatitude());
-            getWeatherInfo(cityName);
-       // }
-//        else {
-//
-//            locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
-//        }
         searchIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String city = cityEdt.getText().toString();
 
-                if(city.isEmpty()){
+                if (city.isEmpty()) {
 
-                    Toast.makeText(MainActivity.this,"please enter city name",Toast.LENGTH_SHORT).show();
-                }
-                else{
+                    Toast.makeText(MainActivity.this, "please enter city name", Toast.LENGTH_SHORT).show();
+                } else {
 
                     cityTV.setText(cityName);
                     getWeatherInfo(city);
@@ -120,13 +118,92 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-//    public void onLocationChanged(Location location) {
-//
-//        locationManager.removeUpdates((LocationListener) this);
-//        cityName = getCityName(location.getLongitude(), location.getLatitude());
-//        getWeatherInfo(cityName);
-//    }
 
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                mfusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            Toast.makeText(MainActivity.this,"latitude"+ location.getLatitude()+"logitude"+location.getLongitude(),Toast.LENGTH_SHORT);
+                            cityName = getCityName(location.getLongitude(), location.getLatitude());
+                            getWeatherInfo(cityName);
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mfusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mfusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+
+            Toast.makeText(MainActivity.this,"latitude"+ mLastLocation.getLatitude()+"logitude"+mLastLocation.getLongitude(),Toast.LENGTH_SHORT);
+            cityName = getCityName(mLastLocation.getLongitude(), mLastLocation.getLatitude());
+            getWeatherInfo(cityName);
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE  );
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+    // If everything is alright then
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -136,41 +213,46 @@ public class MainActivity extends AppCompatActivity {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
 
                 Toast.makeText(this,"Permission granted",Toast.LENGTH_SHORT);
+                getLocation();
             }
             else{
 
                 Toast.makeText(this,"Please provide the permissions",Toast.LENGTH_SHORT);
-                finish();
             }
+        }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLocation();
         }
     }
 
     private String getCityName(double longitude, double latitude){
 
         String cityName = "Not Found";
-        Toast.makeText(MainActivity.this,"latitude"+ latitude+"logitude"+longitude,Toast.LENGTH_SHORT);
+        //Toast.makeText(MainActivity.this,"latitude"+ latitude+"logitude"+longitude,Toast.LENGTH_SHORT);
         Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
 
         try{
 
-            List<Address> addresses = gcd.getFromLocation(latitude,longitude,10);
-            for(Address adr: addresses){
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude,1);
 
-                if(adr != null){
+                if(addresses != null){
 
-                    String city = adr.getLocality();
+                    String city = addresses.get(0).getLocality();
                     if(city != null && !city.equals("")){
 
                         cityName = city;
                     }
                     else{
 
-                        Log.d("TAG","City not found");
-                        Toast.makeText(this,"User city not found,",Toast.LENGTH_SHORT).show();
+//                        Log.d("TAG","City not found");
+//                        Toast.makeText(this,"User city not found,",Toast.LENGTH_LONG).show();
                     }
 
                 }
-            }
 
         }catch (IOException e){
 
@@ -180,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void getWeatherInfo(String cityName){
 
-        String URL = "http://api.weatherapi.com/v1/forecast.json?key=792b6257df8e46bdad754017210309&q="+cityName+"&days=1&aqi=no&alerts=no";
+        String URL = "http://api.weatherapi.com/v1/forecast.json?key=792b6257df8e46bdad754017210309&q=" + cityName + "&days=1&aqi=no&alerts=no";
         cityTV.setText(cityName);
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
@@ -236,5 +318,6 @@ public class MainActivity extends AppCompatActivity {
 
         requestQueue.add(jsonObjectRequest);
     }
+
 
 }
